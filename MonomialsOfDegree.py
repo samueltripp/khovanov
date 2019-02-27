5bff518b53d589c20edc6ad1e2b6f6e9ef1823cd
Sam Tripp <samuel.tripp1@gmail.com>
2019-02-27 09:31:58 -0500

    make this monomial code actually work

--- MonomialsOfDegree.py
+++ MonomialsOfDegree.py
@@ -5,14 +5,18 @@ def genlist(maxdegree, ring):
     if is_QuotientRing(ring):
         ideal = ring.defining_ideal()
     gens = ring.gens()
-    monomials = [ring(1)]
+    monomials = [[ring(1)]]
 
-    for i in range(maxdegree+1):
+    for i in range(1,maxdegree+1):
         degs = WeightedIntegerVectors(i,[1]*len(gens))
+        degreeideal = ring.ideal(0)
+        monomials.append([])
         for part in degs:
             prod = ring(1)
             for j in range(len(gens)):
                 prod *= gens[j]**part[j]
-            if ideal.reduce(prod) != 0 and ideal.reduce(prod) not in monomials:
-                monomials.append(ideal.reduce(prod))                
+            if ideal.reduce(prod) != 0 and ideal.reduce(prod) not in monomials[i]:
+                if degreeideal != ring.ideal(monomials[i]+[ideal.reduce(prod)]):
+                    monomials[i].append(ideal.reduce(prod))
+                    degreeideal = ring.ideal(monomials[i])
     return monomials
