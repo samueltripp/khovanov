fc9f99e92a54b00d8843c4b5911db2344ebcf95b
Shikhin Sethi <draumr.shikhin@gmail.com>
2019-02-28 14:41:23 -0500

    Make generating monomials a bit faster.

--- MonomialsOfDegree.py
+++ MonomialsOfDegree.py
@@ -7,16 +7,16 @@ def genlist(maxdegree, ring):
     gens = ring.gens()
     monomials = [[ring(1)]]
 
-    for i in range(1,maxdegree+1):
-        degs = WeightedIntegerVectors(i,[1]*len(gens))
+    for i in range(1, maxdegree + 1):
+        degs = WeightedIntegerVectors(i, [1] * len(gens))
         degreeideal = ring.ideal(0)
         monomials.append([])
         for part in degs:
             prod = ring(1)
             for j in range(len(gens)):
                 prod *= gens[j]**part[j]
-            if ideal.reduce(prod) != 0 and ideal.reduce(prod) not in monomials[i]:
-                if degreeideal != ring.ideal(monomials[i]+[ideal.reduce(prod)]):
-                    monomials[i].append(ideal.reduce(prod))
-                    degreeideal = ring.ideal(monomials[i])
+            prod = ideal.reduce(prod)
+            if prod != 0 and prod not in degreeideal:
+                monomials[i].append(prod)
+                degreeideal = ring.ideal(monomials[i])
     return monomials
