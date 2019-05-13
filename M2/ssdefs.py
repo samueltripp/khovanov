4a9d2b73120dce2001f30f831cb0c68d62baa76a
zwinkele <zwinkele@gmail.com>
2019-05-13 16:13:40 -0400

    Changed Sage->M2 code, added CKh
    - Made the autogenerated M2 code more efficient by storing the graded pieces instead of the filtered pieces.
    - Added some code to represent E(1,_) as the Khovanov complex.

--- M2/ssdefs.py
+++ M2/ssdefs.py
@@ -1,26 +1,23 @@
 from C2Minus import *
 from pprint import *
 
-# Get a C2Minus object and find the PreFCC
-n = 1
-crossings = (1,)
+n = 2
+crossings = (-2,-2,-2)
 m = len(crossings)
 
+print('Computing C2Minus...')
 C2M = C2Minus(n, crossings)
 pfcc = C2M.preFCC(variant=C2Minus.Variant.BAR)
-
-# Define the base ring and relevant ideals and modules
+print('PreFCC')
 
 R_def = 'R = QQ[{0}]'.format(','.join(pfcc.rings[0,0,0].cover_ring().variable_names()))
 I_defs = {crossing_key: 'I{0} = ideal({1})'.format(str(crossing_key), str(pfcc.rings[crossing_key,0,0].defining_ideal().gens())[1:-1]) for crossing_key in range(2**m)}
 M_defs = {crossing_key: 'M{0} = (R^1)/(I{0}*R^1)'.format(str(crossing_key)) for crossing_key in range(2**m)}
 
-# Define the total complex as a module as a sum of modules at each vertex
-
 M_def = ''
 v_indices = {}
 f_levels = {}
-for i, v in enumerate(pfcc.vertices.keys()):
+for i, v in enumerate(sorted(pfcc.vertices.keys())):
     f_level = pfcc.vertices[v]
     if f_level in f_levels:
         f_levels[f_level].add(v)
@@ -33,8 +30,6 @@ for i, v in enumerate(pfcc.vertices.keys()):
         M_def = 'M = M{}'.format(str(crossing_key))
     else:
         M_def += ' ++ M{}'.format(str(crossing_key))
-		
-# Defining the differential for large objects made my emacs crash so I split it up.
 
 # arrow_strings = set()
 # for e in pfcc.edges:
@@ -56,28 +51,21 @@ next_arrow = arrows_per_def
 while next_arrow < len(arrow_strings):
     del_defs.append('del = del + map(M, M, {{{0}}})'.format(', '.join(arrow_strings[next_arrow:next_arrow+arrows_per_def])))
     next_arrow = next_arrow + arrows_per_def
-	
-# Define the filtration as a list of submodules of M
     
-f_component_strings = []
+Gr_defs = ['Gr = new MutableList']
 
-for f_level in range(0, max(f_levels.keys())+2):
-    f_component_strings.append('ideal(0)*M_0')
+for f_level in range(0, max(f_levels.keys())+1):
+    Gr_defs.append('Gr#{} = 0*M'.format(f_level))
     
 for v, f_level in pfcc.vertices.items():
-    index = v_indices[v]
-    for i in range(0, f_level+1):
-        f_component_strings[i] += ' + R*M_{}'.format(index)
-
-C_def = 'C = [{0}]'.format(', '.join(f_component_strings))
-
-# Print everything for copy/paste into Macaulay2
+    Gr_defs[f_level+1] += ' + R*M_{}'.format(v_indices[v])
 
 print(R_def)
 for k in I_defs:
     print(I_defs[k])
     print(M_defs[k])
 print M_def
+for k in Gr_defs:
+    print k
 for d in del_defs:
     print(d)
-print C_def
